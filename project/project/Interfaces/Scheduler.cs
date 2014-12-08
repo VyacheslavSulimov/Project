1b30df050831fad6f7fd7ae1532d52c90ae69907
Vyacheslav <Sulimov.Vyacheslav@gmail.com>
2014-12-08 10:46:52 +0300

    Создал приблизительный интерфейс для планировщика.

---
+++ project/project/Interfaces/Scheduler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.Interfaces
+{
+   public interface IScheduler
+    {
+      void SetDateTime(DateTime Time);
+      void UpdateDateTime(DateTime OldTime, DateTime NewTime);
+      void DeleteDateTime(DateTime Time);
+      List<DateTime> GetTimes();
+      void Scheduler();
+    }
+}
